91dcd3419f03dbd79a05ca8f50db3f1e96308089
jerry <juanino@gmail.com>
2019-05-25 02:47:23 +0100

    added webhook for restarting wrapper and dm channel id

--- shopbotconfig_example.py
+++ shopbotconfig_example.py
@@ -2,3 +2,5 @@
 # when the bot is created
 
 token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+webhook_safe = 'https://discordapp.com/api/webhooks/XXXX/XXXX'
+bot_channel_id = '##################'
